90c2c60e4b7b19dd23566ac985cfdc24d439c41b
JuliandersonSa <juliandersonsantossa@gmail.com>
2025-06-20 12:32:42 -0300

    Adicionados gráfico e PDF de análise final

--- scripts/monitor_internet.py
+++ scripts/monitor_internet.py
@@ -1,1 +1,16 @@
+import matplotlib.pyplot as plt
+import pandas as pd
 
+df_12 = pd.read_csv("dados/velocidade_12_do_06_2025.csv")
+df_14 = pd.read_csv("dados/velocidade_14_do_06_2025.csv")
+
+plt.figure(figsize=(10, 6))
+plt.plot(pd.to_datetime(df_12["DataHora"]), df_12["Download_Mbps"], label="Download 12/06", color="blue")
+plt.plot(pd.to_datetime(df_14["DataHora"]), df_14["Download_Mbps"], label="Download 14/06", color="green")
+plt.title("Velocidade de Download (Mbps)")
+plt.xlabel("Horário")
+plt.ylabel("Download")
+plt.legend()
+plt.grid(True)
+plt.tight_layout()
+plt.savefig("scripts/grafico_download.png")
